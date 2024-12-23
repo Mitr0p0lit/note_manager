9401b6c383cbb1c08f16758d6bb53ea3a01135db
Mitr0p0lit <dorogay7856@gmail.com>
2024-12-23 16:38:57 +0300

    homework

---
+++ main.py
@@ -0,0 +1,12 @@
+username = "Mitr0p0lit"
+title = "Заголовок"
+content = "Описание"
+status = True #не знаю какой статус поставить, пусть будет будто активна
+created_date = "23-12-2024"
+issue_date = "23-12-2025"
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Описание:", content)
+print("Статус:", status)
+print("Дата создания:", created_date)
+print("Дата окончания:", issue_date)
