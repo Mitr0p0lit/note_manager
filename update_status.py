dca7c3984faaf50fc7f27232c73dfdc45c4cbd8b
Mitr0p0lit <dorogay7856@gmail.com>
2024-12-31 01:01:15 +0300

    homework

---
+++ update_status.py
@@ -0,0 +1,6 @@
+status = False
+print("Статус: ", status)
+x = input("Хотите изменить статус? (да/нет): ")
+if x == "да":
+    status = input("Введите статус: ")
+    print("Новый cтатус!: ", status)
