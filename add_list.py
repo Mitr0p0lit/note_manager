b345cb00f3593ebba76507b3e977c12145483841
Mitr0p0lit <dorogay7856@gmail.com>
2025-01-24 15:40:39 +0300

    homework

--- add_list.py
+++ add_list.py
@@ -1,7 +1,7 @@
-x = 0
-x = int(input("Введите кол-во заголовков: "))
-Headlines = [0] * x
-for i in range(x):
+title_count = 0
+title_count = int(input("Введите кол-во заголовков: "))
+Headlines = [0] * title_count
+for i in range(title_count):
     Headlines [i] = input("Введите заголовок: ")
 print(Headlines)
 
